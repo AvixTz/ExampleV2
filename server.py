4d1c869e56da6308fe4f7d2e9f65e2a1fc187b3d
AvixTz <75776095+AvixTz@users.noreply.github.com>
2025-09-30 15:21:51 +0300

    Update server.py

--- server.py
+++ server.py
@@ -197,4 +197,5 @@ if __name__ == '__main__':
     print("="*60)
     print("\n📱 Open in browser: http://localhost:5000")
     print("\n⚡ Server is running... Press Ctrl+C to stop\n")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    app.run(debug=False, host='0.0.0.0', port=port)
